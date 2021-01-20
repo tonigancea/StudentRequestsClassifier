e8cf9bd0e6c79a57f75ede566567af92db44c0df
Andrei Mihu <andreimihu28097@gmail.com>
2021-01-20 07:35:10 -0500

    html update

--- src/main/java/com/technocratsid/controller/WordCountController.java
+++ src/main/java/com/technocratsid/controller/WordCountController.java
@@ -5,23 +5,27 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
 import com.technocratsid.service.WordCountService;
 
-@RestController
+@Controller
 public class WordCountController {
 
 	@Autowired
 	WordCountService service;
 
-	@RequestMapping(method = RequestMethod.POST, path = "/wordcount")
+	@GetMapping(path = "/wordcount")
 	public Map<String, Long> count(@RequestParam(required = false) String words) {
 		List<String> wordList = Arrays.asList(words.split("\\|"));
 		return service.getCount(wordList);
+	}
+
+	@GetMapping(path = "/cerere")
+	public String index() {
+		return "cerere";
 	}
 
 }
